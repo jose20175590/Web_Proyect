615722311ebcb5a217652db787c6826162de3d3c
Enmanuel Perez <Enmanuel_21_@hotmail.com>
2019-07-28 20:09:43 -0400

    Creacion de Proyecto

---
+++ Web_API/Controllers/EstudianteController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Service;
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class EstudianteController : ControllerBase
+    {
+        private readonly IEstudianteService _estudianteService;
+        public EstudianteController(IEstudianteService estudianteService)
+        {
+            _estudianteService = estudianteService;
+        }
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_estudianteService.GetAll());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(
+                _estudianteService.Get(id)
+            );
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] Estudiante model)
+        {
+            return Ok(
+                _estudianteService.Add(model)
+                );
+        }
+
+        // PUT api/values/5
+        [HttpPost]
+        public IActionResult Put([FromBody] Estudiante model)
+        {
+            return Ok(
+                _estudianteService.Add(model)
+                );
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok(
+                _estudianteService.Delete(id)
+                );
+        }
+    }
+}
