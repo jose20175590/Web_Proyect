7197cd12f5d5fb5f5c3846bc6162987d06a1638e
Enmanuel Perez <Enmanuel_21_@hotmail.com>
2019-07-28 20:21:57 -0400

    Agregando Estructura

---
+++ Persistence/EstudianteDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class EstudianteDbContext : DbContext
+    {
+        public DbSet <Estudiante> Estudiante { get; set; }
+
+        public EstudianteDbContext(DbContextOptions<EstudianteDbContext> opciones) : base(opciones)
+        { }
+    }
+}
---
+++ Persistence/Migrations/20190728224022_Initialize.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class Initialize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estudiante",
+                columns: table => new
+                {
+                    estudianteId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    nombre = table.Column<string>(nullable: true),
+                    apellido = table.Column<string>(nullable: true),
+                    curso = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiante", x => x.estudianteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estudiante");
+        }
+    }
+}
